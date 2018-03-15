ba67d5ff0f5962e4c318362dad4ef0fd2138fc45
Liam O Gorman <L.ogorman@mycit.ie>
2018-03-15 22:45:25 +0000

    Added custom bootstrap theme, removed secret key from settings.py

--- DjangoCalendar/settings.py
+++ DjangoCalendar/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@irvpm!ci1-a4nxtaepnl7mk2=rzr3uy0g)r5ofi-t8+skvp&_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
